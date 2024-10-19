5b7261aaa9467ffbfd3b3762570a89364fae5a1f
Ganesh <ganesh2002aug13@gmail.com>
2024-10-19 11:28:10 +0530

    commit

---
+++ DOM/app.js
@@ -0,0 +1,11 @@
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("div a"));
+// console.dir(document.querySelectorAll("div a"));
+
+// let links = document.querySelectorAll(".box a");
+// for (link of links) {
+//     link.style.color = "orange";
+// }
+// for (let i = 0; i < links.length; i++) {
+//     links[i].style.color = "green";
+// }
